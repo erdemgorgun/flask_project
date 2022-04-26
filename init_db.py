98e3be1ce54847c2f8d525ce07955a390d805bf8
Erdem Görgün <egorgun@ford.com.tr>
2022-04-26 22:45:37 +0300

    initial commit

--- init_db.py
+++ init_db.py
@@ -9,11 +9,15 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
+            ('customer 1', 'Port: 23   Vlan Id: 56   IPv6 Address/Prefix Length: fe80::224:97ff:fe69:69af/64')
             )
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
+            ('customer 1', 'Port: 23   Vlan Id: 56   IPv6 Address/Prefix Length: 2001:9:10:56::60/64')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('another customer', 'Port:42 Vlan Id: N/A IPv6 Address/Prefix Length: fe80::224:97ff:fe69:69a1/64')
             )
 
 connection.commit()
